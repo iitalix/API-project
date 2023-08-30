8603a25c990434c752ef8771eeded7b78f5939a2
Lan <nolan.oribello@gmail.com>
2023-08-30 15:17:24 -0700

    Initialize Express and Sequelize with CSRF protection

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,46 @@
-const express = require("express");
-require("express-async-errors");
-const morgan = require("morgan");
-const cors = require("cors");
-const csurf = require("csurf");
-const helmet = "helmet";
-const cookieParser = require("cookie-parser");
+// backend/app.js
 
-const {environment} = require("config");
-const isProduction = environment === "production";
-const app = express();
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 
-app.use(morgan("dev"));
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+const app = express();
+
+app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
-app.use(routes);
 
 // -- Security Middleware --
 if (!isProduction) {
-  // enable CORS only in development
+  // enable cors only in development
   app.use(cors());
 }
 
 // helmet helps set a variety of headers to better secure the app
 app.use(
   helmet.crossOriginResourcePolicy({
-
-    policy: "cross-origin",
+    policy: "cross-origin"
   })
 );
 
-// Sets the _csrf token and creates req.csrfToken method
+// Sets the _csrf token and create req.csrfToken method
 app.use(
   csurf({
     cookie: {
       secure: isProduction,
       sameSite: isProduction && "Lax",
-      httpOnly: true,
-    },
+      httpOnly: true
+    }
   })
 );
+
+app.use(routes);
+
+module.exports = app;
