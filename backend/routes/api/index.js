dc17f8356da478c57dd2098157076ad5651b3f78
Lan <nolan.oribello@gmail.com>
2023-08-31 20:06:34 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,9 +2,17 @@
 const router = require("express").Router();
 const {setTokenCookie} = require("../../utils/auth");
 const {User} = require("../../db/models");
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const {restoreUser} = require("../../utils/auth.js");
 const { requireAuth } = require('../../utils/auth.js');
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
 router.use(restoreUser);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 // GET /api/restore-user
 router.get("/restore-user", (req, res) => {
@@ -30,5 +38,9 @@ router.get(
     return res.json(req.user);
   }
 );
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
---
+++ backend/routes/api/session.js
@@ -0,0 +1,54 @@
+// backend/routes/api/session.js
+const express = require("express");
+const {Op} = require("sequelize");
+const bcrypt = require("bcryptjs");
+
+const {setTokenCookie, restoreUser} = require("../../utils/auth");
+const {User} = require("../../db/models");
+
+const router = express.Router();
+
+// Log in
+router.post("/", async (req, res, next) => {
+
+  // parse the credentials from the request body
+  const {credential, password} = req.body;
+
+  //turn off defaultScope and query for the user based on credentials
+  const user = await User.unscoped().findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential,
+      },
+    },
+  });
+
+  // if user is not found, or password hash does not match, create login failed error
+  // bcrypt.compareSync compares req.body pw with hashed pw
+  if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+    const err = new Error("Login failed");
+    err.status = 401;
+    err.title = "Login failed";
+    err.errors = {credential: "The provided credentials were invalid."};
+    return next(err);
+  }
+
+  // if user password correct:
+  // (user's non-sensitive information)
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  // set token cookie and return response
+  // with user's non-sensitive information
+  await setTokenCookie(res, safeUser);
+
+  return res.json({
+    user: safeUser,
+  });
+});
+
+module.exports = router;
