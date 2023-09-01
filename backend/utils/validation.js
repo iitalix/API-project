58e0f5837302347cf8dbd5271e50ef263140836e
Lan <nolan.oribello@gmail.com>
2023-08-31 21:28:24 -0700

    Add Validation middleware to check for formatting errors

---
+++ backend/utils/validation.js
@@ -0,0 +1,27 @@
+// backend/utils/validation.js
+const { validationResult } = require('express-validator');
+
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  // if there are no validation errors, invoke the middleware
+  if (!validationErrors.isEmpty()) {
+    const errors = {};
+    validationErrors
+      .array()
+      .forEach(error => errors[error.path] = error.msg);
+
+    const err = Error("Bad request.");
+    err.errors = errors;
+    err.status = 400;
+    err.title = "Bad request.";
+    next(err);
+  }
+  next();
+};
+
+module.exports = {
+  handleValidationErrors
+};
