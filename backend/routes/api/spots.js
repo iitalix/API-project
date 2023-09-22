0a2809310983e650f6263489ef3d1bea6a6c5a2c
Lan <nolan.oribello@gmail.com>
2023-09-21 22:24:12 -0700

    completed Phase 1

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -651,6 +651,7 @@ router.post("/:spotId/bookings", requireAuth, async (req, res, next) => {
 
   // Pre-Existing Booking Conflicts
   for (let booking of spotBookingsArr) {
+
     // Existing Start Date
     const bookingStartDate = new Date(booking.startDate);
     const reservedStartDate = bookingStartDate.getTime();
