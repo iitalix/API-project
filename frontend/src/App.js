60e620979a5a5e0989763a6053592de18220176d
Lan <nolan.oribello@gmail.com>
2023-09-22 14:41:26 -0700

    completed Signup Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
 function App() {
@@ -17,9 +18,15 @@ function App() {
   return (
     isLoaded && (
       <Switch>
+
         <Route path="/login">
           <LoginFormPage />
         </Route>
+
+        <Route path="/signup">
+          <SignupFormPage />
+        </Route>
+
       </Switch>
     )
   );
