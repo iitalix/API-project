8c7da2d2c636288b19c095f39b1f0a535487802f
Lan <nolan.oribello@gmail.com>
2023-09-25 09:58:39 -0700

    completed basics of Spot Info Container

--- frontend/src/components/SpotDetailsPage/index.js
+++ frontend/src/components/SpotDetailsPage/index.js
@@ -3,9 +3,8 @@
 import {useSelector, useDispatch} from "react-redux";
 import {useParams} from "react-router-dom";
 import {thunkGetSpotDetails} from "../../store/spots";
-import "./SpotDetailsPage.css"
-import { useEffect } from "react";
-
+import "./SpotDetailsPage.css";
+import {useEffect} from "react";
 
 export default function SpotDetailsPage() {
   const dispatch = useDispatch();
@@ -13,11 +12,17 @@ export default function SpotDetailsPage() {
   const spot = useSelector((state) => state.spots.spotDetails);
 
   useEffect(() => {
-
-    dispatch(thunkGetSpotDetails(spotId))
-  }, [])
+    dispatch(thunkGetSpotDetails(spotId));
+  }, []);
 
   if (!spot.id) return null;
+
+  // TODO: NOT WORKING
+  function resAlert(e) {
+
+    e.preventDefault();
+    alert("Feature coming soon!")
+  }
 
   const fourImagesArr = spot.SpotImages.slice(1);
   return (
@@ -30,7 +35,11 @@ export default function SpotDetailsPage() {
       </div>
       <div id="images-container">
         <div>
-          <img src={spot.SpotImages[0].url} alt="interior room" id="main-image" />
+          <img
+            src={spot.SpotImages[0].url}
+            alt="interior room"
+            id="main-image"
+          />
         </div>
         <div id="side-image-container">
           {fourImagesArr.map((image) => (
@@ -43,12 +52,25 @@ export default function SpotDetailsPage() {
           ))}
         </div>
       </div>
-      <div>
-        <div>
-          <div>
+      <div id="spot-info-container">
+        <div id="spot-description">
+          <div id="host-info">
             Hosted by {spot.User.firstName} {spot.User.lastName}
           </div>
           <div>{spot.description}</div>
+        </div>
+        <div id="callout-box">
+
+            <div id="callout-box-upper">
+              <div>{`$${spot.price} night`}</div>
+              <div>
+                <i className="fa-solid fa-star"></i>
+                {spot.avgRating}
+                <div>{spot.numReviews} reviews</div>
+            </div>
+
+        </div>
+          <button onclick={resAlert}>Reserve</button>
         </div>
       </div>
     </>
